0eb83db9d976af7109680d8e88da671f46090058
malloyxS <tropiceskijivan@gmail.com>
2025-02-11 11:27:58 +0300

    11/02/2025 actual code

---
+++ IT-web/JS/main_js.js
@@ -0,0 +1,9 @@
+//Swiper code
+var swiper = new Swiper(".home", { //.home - class
+        spaceBetween:  30,
+        centeredSlides: true,
+        navigation:{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
